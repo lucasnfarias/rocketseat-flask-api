d1d4af8658316aa1c4743d3f5a88a1e375182f05
Lucas Farias <lucas.farias@rtbhouse.com>
2025-04-24 20:10:53 +0000

    feat: create calculator 2

---
+++ src/calculators/calculator_2_test.py
@@ -0,0 +1,28 @@
+from pytest import raises
+from .calculator_2 import Calculator2
+from typing import Dict
+
+class MockRequest:
+  def __init__(self, body: Dict) -> None:
+    self.json = body
+
+def test_calculate():
+  mock_request = MockRequest({
+    "numbers": [2.12, 4.62, 1.32]
+  })
+
+  calculator_2 = Calculator2()
+  response = calculator_2.calculate(mock_request)
+
+  assert isinstance(response, dict)
+  assert response == { 'data': { 'Calculator': 2, 'result': 0.08 } }
+
+
+def test_calculate_with_body_error():
+  mock_request = MockRequest(body={ "wrong": 1 })
+  calculator_1 = Calculator2()
+
+  with raises(Exception) as exc_info:
+    calculator_1.calculate(mock_request)
+
+  assert str(exc_info.value) == "Invalid fields."
