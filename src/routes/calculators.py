ef137cd68c9a8f1c851d88c33a257a0af5d5ca6a
Lucas Farias <lucas.farias@rtbhouse.com>
2025-04-25 18:43:50 +0000

    feat: create calculator 3 route

--- src/routes/calculators.py
+++ src/routes/calculators.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 
 from src.factories.calculator1_factory import calculator1_factory
 from src.factories.calculator2_factory import calculator2_factory
-from src.calculators.calculator_1 import Calculator1
+from src.factories.calculator3_factory import calculator3_factory
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
@@ -16,6 +16,13 @@ def calculator_1():
 @calc_route_bp.route("/calculators/2", methods=["GET"])
 def calculator_2():
   calc = calculator2_factory()
+  response = calc.calculate(request)
+
+  return jsonify(response)
+
+@calc_route_bp.route("/calculators/3", methods=["GET"])
+def calculator_3():
+  calc = calculator3_factory()
   response = calc.calculate(request)
 
   return jsonify(response)
